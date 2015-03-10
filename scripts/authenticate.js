03f1ee90730034fa644dbc755ecf42e89171c1b0
Ryan <ryan@ryan.com>
2015-03-10 17:23:26 -0600

    worked on authenticate with the SE javascript sdk

---
+++ scripts/authenticate.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+   SE.init({
+       clientId: 4422,
+       key: '2FgrF56*JKZBJ43DniQc7A((',
+       channelUrl: 'http://tk-rand.github.io/blizzard_overflow/blank',
+       complete: function(data){
+           console.log(data);
+       }
+   }); 
+});
+
+$('#auth-button').click(function(){
+   SE.authenticate({
+      success: function(data){
+          sessionStorage.setItem('accessToken', data.accessToken);
+          sessionStorage.setItem('experationDate', data.experationDate);
+      }, 
+      scope: ['write_access', 'private_info'],
+      error: function(data){
+          alert("The following error occured: " + data.errorName + 
+                "\n The server says: " +  data.errorMessage);
+      } 
+   }); 
+});
