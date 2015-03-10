3e4033a022ad9067167b634f750d0b8603b1d95b
ryanearl@suumail.net <ryan@ryan.com>
2015-03-10 17:43:20 -0600

    made a mistake, had the click handler outside the document.ready function

--- scripts/authenticate.js
+++ scripts/authenticate.js
@@ -1,24 +1,26 @@
-$(document).ready(function(){
-   SE.init({
-       clientId: 4422,
-       key: '2FgrF56*JKZBJ43DniQc7A((',
-       channelUrl: 'http://tk-rand.github.io/blizzard_overflow/blank',
-       complete: function(data){
-           console.log(data);
-       }
-   }); 
-});
+$(document).ready(function() {
+    SE.init({
+        clientId : 4422,
+        key : '2FgrF56*JKZBJ43DniQc7A((',
+        channelUrl : 'http://tk-rand.github.io/blizzard_overflow/blank',
+        complete : function(data) {
+            console.log(data);
+        }
+    });
 
-$('#auth-button').click(function(){
-   SE.authenticate({
-      success: function(data){
-          sessionStorage.setItem('accessToken', data.accessToken);
-          sessionStorage.setItem('experationDate', data.experationDate);
-      }, 
-      scope: ['write_access', 'private_info'],
-      error: function(data){
-          alert("The following error occured: " + data.errorName + 
-                "\n The server says: " +  data.errorMessage);
-      } 
-   }); 
-});
+    $('#auth-button').click(function() {
+        SE.authenticate({
+            success : function(data) {
+                sessionStorage.setItem('accessToken', data.accessToken);
+                sessionStorage.setItem('experationDate', data.experationDate);
+            },
+            scope : ['write_access', 'private_info'],
+            error : function(data) {
+                alert("The following error occured: " + data.errorName + "\n The server says: " + data.errorMessage);
+            }
+        });
+    });
+
+}); 
+
+
