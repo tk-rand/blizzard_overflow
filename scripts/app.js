2b1baf2c958706a8ae924f0ebbee6304e7e8dc68
ryanearl@suumail.net <ryan@ryan.com>
2015-03-13 11:28:25 -0600

    started working on search api

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
     var profile = JSON.parse(sessionStorage.getItem('user-profile'));
     $('#user-name').text(profile.display_name);
-    $('#user-rep').text(profile.reputation);    
+    $('#user-rep').text(profile.reputation);
+    
+    $('#question-search').keypress(function(event){
+        if(event.keyCode === 13){
+            console.log(target);
+            useSearchApi(event.target.value);    
+        }
+    });   
 });
+
+function useSearchApi(query){
+    console.log(query);
+}
