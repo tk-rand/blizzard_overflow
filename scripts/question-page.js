ae66b6206e8ef85a1b9f9f61e59b8d911bf90dc6
ryanearl@suumail.net <ryan@ryan.com>
2015-03-15 22:24:45 -0600

    ititial work on making the quesitons show up properly on question page

--- scripts/question-page.js
+++ scripts/question-page.js
@@ -7,12 +7,16 @@ $(document).ready(function() {
 
     useQuestionApi(questionId);
 
+
+    $('.back-button').click(function(){
+        window.location.href = "http://tk-rand.github.io/blizzard_overflow/search-results.html";    
+    });
 });
 
 function useQuestionApi(questionId) {
     var data = {
         'site' : 'stackoverflow',
-        'filter' : '!3yXvh3jvaHG)DFsal',
+        'filter' : '!)rCcH8Re8h1Fh)8)SGj)',
         'key' : sessionStorage.getItem('key'),
         'access_token' : sessionStorage.getItem('accessToken')
     };
@@ -30,4 +34,24 @@ function useQuestionApi(questionId) {
 
 function createQuestion(data){
     console.log(data);
+    
+    $('.question-title h2').html(data.items[0].title);
+    $('.question-score').text(data.items[0].score);
+    $('#view-count').text(data.items[0].view_count);
+    $('.question-body').prepend(data.items[0].body);
+    $('#creation-date').text(moment.unix(data.items[0].creation_date).format('MM-Do-YYYY'));
+    
+    data.items[0].tags.forEach(function(tag){
+        var display = "<div class='tag-container'>"+ tag +"</div>";
+        $('.question-tags').append(display);
+    });
+    
+    data.items[0].comments.forEach(function(comment){
+        var display = "<div class='comment-container'><span class='comment-score'>"+ comment.score +"</span>;
+        display += "<span class='comment-owner-name'>"+ comment.owner.display_name +"</span>";
+        display += "<div class='comment-body'>"+ comment.body +"</div><span class='comment-creation-date'>"+ comment.creation_date +"</span></div>";
+        
+        $('.question-comments').append(display);       
+    });
+        
 }
